254cea04257e4b46c1607675a0d50a68fdb8fff2
mikezhang <154245916@qq.com>
2025-06-16 14:44:38 +0900

    Update orders.ts

--- src/mock/orders.ts
+++ src/mock/orders.ts
@@ -1,7 +1,7 @@
 // src/mock/orders.ts
 export const mockOrders = [
-	{ id: 1, productName: "iPhone 15", quantity: 2 },
-	{ id: 2, productName: "MacBook Pro", quantity: 1 },
-	{ id: 3, productName: "iPad Air", quantity: 3 },
-	{ id: 4, productName: "AirPods Pro", quantity: 5 },
+	{ id: 1, productName: "iPhone 15", quantity: 2, orderStatus: 0 },
+	{ id: 2, productName: "MacBook Pro", quantity: 1, orderStatus: 1 },
+	{ id: 3, productName: "iPad Air", quantity: 3 , orderStatus: 2 },
+	{ id: 4, productName: "AirPods Pro", quantity: 0 },
 ];
